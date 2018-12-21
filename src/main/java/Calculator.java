a5b5ac0a8d097a82d497c7d7d70dfb96ef35f5e2
student <student@tc.main>
2018-12-21 20:54:50 +0200

    Initial commit Lab1

---
+++ src/main/java/Calculator.java
@@ -0,0 +1,52 @@
+/**
+ * Created by kate on 1/12/16.
+ */
+
+public class Calculator {
+
+    public static void main(String[] args) {
+    }
+
+    public int add(int a, int b) {
+
+        return a + b;
+    }
+
+    public int multiply(int a, int b) {
+
+        return a * b;
+    }
+
+    public double division(int a, int b) {
+
+        return a / b;
+
+    }
+    public int subtraction (int a, int b) {
+
+
+        return a - b;
+    }
+
+    public double squareRoot (int a) {
+
+        return Math.sqrt(a);
+
+    }
+
+    public double xPov2 (int a) {
+
+        return Math.pow(a,2);
+    }
+
+    //TODO
+    //add methods for multiply, division, subtraction, square root, x^2
+
+}
+
+
+
+
+
+
+
